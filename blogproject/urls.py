c5aa3218bb878b26018d5833b7182ec3f93e5e02
donghapark <donghaha12@naver.com>
2023-05-09 18:57:25 +0900

    로그인 로그아웃

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from blogapp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +32,7 @@ urlpatterns = [
     path('blogapp/', include('blogapp.urls')), 
 
     path('common/', include('common.urls')),
+
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
